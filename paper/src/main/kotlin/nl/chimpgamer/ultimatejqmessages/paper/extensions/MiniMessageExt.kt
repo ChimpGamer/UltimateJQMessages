aa40f55110ae006e03fa0faab563cee8f8586a51
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-06-18 13:51:11 +0200

    No need for this method

--- paper/src/main/kotlin/nl/chimpgamer/ultimatejqmessages/paper/extensions/MiniMessageExt.kt
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatejqmessages/paper/extensions/MiniMessageExt.kt
@@ -5,7 +5,6 @@ import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.minimessage.MiniMessage.miniMessage
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
-import nl.chimpgamer.ultimatejqmessages.paper.models.User
 import org.bukkit.entity.Player
 
 fun String.parse() = miniMessage().deserialize(this).decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE)
@@ -25,9 +24,3 @@ fun Map<String, *>.toTagResolver(parsed: Boolean = false) = TagResolver.resolver
 )
 
 internal fun getDisplayNamePlaceholder(player: Player) = Placeholder.component("displayname", player.displayName())
-
-internal fun getQuitMessagePlaceholders(player: Player, user: User) = TagResolver.builder()
-    .resolver(getDisplayNamePlaceholder(player))
-
-    .resolver(Placeholder.parsed("custom_quit_message", user.customQuitMessage ?: ""))
-    .build()
