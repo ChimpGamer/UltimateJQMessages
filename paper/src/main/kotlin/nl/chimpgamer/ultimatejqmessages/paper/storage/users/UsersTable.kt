1f025f26dd8d71cf556e9ccc2efeb2f4dadb3f8c
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-10-02 15:25:34 +0200

    Fixed error when trying to delete join quit message while it was still in use by a player. (Needs table regeneration to apply)

--- paper/src/main/kotlin/nl/chimpgamer/ultimatejqmessages/paper/storage/users/UsersTable.kt
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatejqmessages/paper/storage/users/UsersTable.kt
@@ -3,11 +3,12 @@ package nl.chimpgamer.ultimatejqmessages.paper.storage.users
 import nl.chimpgamer.ultimatejqmessages.paper.storage.joinquitmessages.JoinQuitMessagesTable
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ReferenceOption
 
 object UsersTable : UUIDTable("users", "uuid") {
     val playerName: Column<String> = varchar("player_name", 16)
-    val joinMessage = reference("join_message", JoinQuitMessagesTable).nullable()
-    val quitMessage = reference("quit_message", JoinQuitMessagesTable).nullable()
+    val joinMessage = reference("join_message", JoinQuitMessagesTable, onDelete = ReferenceOption.SET_NULL).nullable()
+    val quitMessage = reference("quit_message", JoinQuitMessagesTable, onDelete = ReferenceOption.SET_NULL).nullable()
     val customJoinMessage = text("custom_join_message").nullable()
     val customQuitMessage = text("custom_quit_message").nullable()
     val showJoinQuitMessages = bool("show_join_quit_messages").default(true)
