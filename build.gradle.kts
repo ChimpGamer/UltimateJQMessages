465b83210aa8eba8a8dd27eb6d973e14213c55cb
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-10-02 15:26:21 +0200

    Bump RyseInventory to 1.6.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ subprojects {
         compileOnly(kotlin("stdlib"))
 
         compileOnly("dev.dejvokep:boosted-yaml:1.3.1")
-        implementation("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.6")
+        implementation("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.7")
         compileOnly("cloud.commandframework:cloud-paper:1.8.4")
         compileOnly("cloud.commandframework:cloud-minecraft-extras:1.8.4")
 
