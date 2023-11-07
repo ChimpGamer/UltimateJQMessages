96183bca8ac64398b18430d9549fa40128b35101
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-11-07 18:55:20 +0100

    Added MiniPlaceholders support

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -7,6 +7,9 @@ dependencies {
 
     implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.12.1") { isTransitive = false }
     implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.12.1") { isTransitive = false }
+
+    compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.2.2")
+    compileOnly("io.github.miniplaceholders:miniplaceholders-kotlin-ext:2.2.2")
 }
 
 tasks {
---
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatejqmessages/paper/hooks/MiniPlaceholdersHook.kt
@@ -0,0 +1,45 @@
+package nl.chimpgamer.ultimatejqmessages.paper.hooks
+
+import io.github.miniplaceholders.api.Expansion
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.tag.Tag
+import nl.chimpgamer.ultimatejqmessages.paper.UltimateJQMessagesPlugin
+import org.bukkit.entity.Player
+
+class MiniPlaceholdersHook(private val plugin: UltimateJQMessagesPlugin) {
+    private val name = "MiniPlaceholders"
+    private val isLoaded get() = plugin.server.pluginManager.isPluginEnabled(name)
+
+    private lateinit var expansion: Expansion
+
+    fun load() {
+        if (!isLoaded) return
+        val joinQuitMessagesHandler = plugin.joinQuitMessagesHandler
+        expansion = Expansion.builder("ultimatejqmessages")
+            .filter(Player::class.java)
+            .audiencePlaceholder("show_join_quit_messages") { audience, _, _ ->
+                audience as Player
+                val user = plugin.usersHandler.getIfLoaded(audience.uniqueId) ?: return@audiencePlaceholder null
+                return@audiencePlaceholder Tag.selfClosingInserting(Component.text(user.showJoinQuitMessages))
+            }
+            .audiencePlaceholder("has_join_message_selected") { audience, _, _ ->
+                audience as Player
+                val user = plugin.usersHandler.getIfLoaded(audience.uniqueId) ?: return@audiencePlaceholder null
+                return@audiencePlaceholder Tag.selfClosingInserting(Component.text(user.joinMessage != null))
+            }
+            .audiencePlaceholder("has_quit_message_selected") { audience, _, _ ->
+                audience as Player
+                val user = plugin.usersHandler.getIfLoaded(audience.uniqueId) ?: return@audiencePlaceholder null
+                return@audiencePlaceholder Tag.selfClosingInserting(Component.text(user.quitMessage != null))
+            }
+            .build()
+
+        expansion.register()
+    }
+
+    fun unload() {
+        if (this::expansion.isInitialized) {
+            expansion.unregister()
+        }
+    }
+}
