6c5096c48f4b26ba86d6bf65996fc273897cc985
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-04-29 18:50:54 +0200

    Bump mariadb-java-client to 3.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ subprojects {
 
         compileOnly("com.zaxxer:HikariCP:5.1.0")
         compileOnly("org.xerial:sqlite-jdbc:3.44.1.0")
-        compileOnly("org.mariadb.jdbc:mariadb-java-client:3.3.1")
+        compileOnly("org.mariadb.jdbc:mariadb-java-client:3.3.3")
     }
 
     java {
--- paper/src/main/java/nl/chimpgamer/ultimatejqmessages/paper/UltimateJQMessagesLoader.java
+++ paper/src/main/java/nl/chimpgamer/ultimatejqmessages/paper/UltimateJQMessagesLoader.java
@@ -21,7 +21,7 @@ public class UltimateJQMessagesLoader implements PluginLoader {
             add("org.jetbrains.exposed:exposed-dao:0.49.0");
             add("org.jetbrains.exposed:exposed-jdbc:0.49.0");
             add("org.xerial:sqlite-jdbc:3.44.1.0");
-            add("org.mariadb.jdbc:mariadb-java-client:3.3.1");
+            add("org.mariadb.jdbc:mariadb-java-client:3.3.3");
             add("cloud.commandframework:cloud-paper:1.8.4");
             add("cloud.commandframework:cloud-minecraft-extras:1.8.4");
             add("cloud.commandframework:cloud-kotlin-coroutines:1.8.4");
