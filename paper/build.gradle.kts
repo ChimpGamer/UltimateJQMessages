bd9be73487e05a3fc74f35fa83dd84a272660349
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-10-10 17:14:50 +0200

    Bump mccoroutine to 2.20.0

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -5,8 +5,8 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
 
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-folia-api:2.18.0") { isTransitive = false }
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-folia-core:2.18.0") { isTransitive = false }
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-folia-api:2.20.0") { isTransitive = false }
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-folia-core:2.20.0") { isTransitive = false }
 
     compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.2.3")
     compileOnly("io.github.miniplaceholders:miniplaceholders-kotlin-ext:2.2.3")
