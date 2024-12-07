a21038084254747e17ce75de6ad96cc5384d9e36
ChimpGamer <tim.cremers@ziggo.nl>
2024-12-07 20:34:50 +0100

    Bump mariadb-java-client to 3.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ subprojects {
 
         compileOnly("com.zaxxer:HikariCP:6.2.1")
         compileOnly("org.xerial:sqlite-jdbc:3.47.1.0")
-        compileOnly("org.mariadb.jdbc:mariadb-java-client:3.4.1")
+        compileOnly("org.mariadb.jdbc:mariadb-java-client:3.5.1")
     }
 
     java {
