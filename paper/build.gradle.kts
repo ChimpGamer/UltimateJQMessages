b114e1c67733d268c5431b6624cfd1f4c9500a1a
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-12-17 17:13:05 +0100

    Bump RyseInventory-Plugin to 1.6.15

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 
     implementation("com.github.shynixn.mccoroutine:mccoroutine-folia-api:2.20.0") { isTransitive = false }
     implementation("com.github.shynixn.mccoroutine:mccoroutine-folia-core:2.20.0") { isTransitive = false }
-    implementation("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.13")
+    implementation("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.15")
 
     compileOnly("org.incendo:cloud-paper:2.0.0-beta.10")
     compileOnly("org.incendo:cloud-minecraft-extras:2.0.0-beta.10")
@@ -18,8 +18,6 @@ dependencies {
     compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.2.3")
     compileOnly("io.github.miniplaceholders:miniplaceholders-kotlin-ext:2.2.3")
 }
-
-
 
 tasks {
     jar {
