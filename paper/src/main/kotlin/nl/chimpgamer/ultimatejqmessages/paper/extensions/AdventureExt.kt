5973d6a913d831b162b45a45b60700a2d4cf7347
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2025-12-05 15:58:06 +0100

    Added Component#toPlainText method

--- paper/src/main/kotlin/nl/chimpgamer/ultimatejqmessages/paper/extensions/AdventureExt.kt
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatejqmessages/paper/extensions/AdventureExt.kt
@@ -4,12 +4,15 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.ComponentIteratorType
 import net.kyori.adventure.text.TextComponent
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import net.kyori.adventure.title.Title
 import java.time.Duration
 
 private val legacyComponentSerializer = LegacyComponentSerializer.builder().character('&').hexColors().build()
 
 fun Component.toLegacy() = legacyComponentSerializer.serialize(this)
+
+fun Component.toPlainText() = PlainTextComponentSerializer.plainText().serialize(this)
 
 fun Component.length() = iterable(ComponentIteratorType.DEPTH_FIRST)
     .filterIsInstance<TextComponent>()
